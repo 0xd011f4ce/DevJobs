327c8f0972e13ab769c7bff7b61ec00f89ef8c9b
lain <lyx@lyxincyberia.org>
2024-09-25 17:40:13 -0500

    adding the fields

--- resources/views/livewire/create-vacancy.blade.php
+++ resources/views/livewire/create-vacancy.blade.php
@@ -1,3 +1,54 @@
-<div>
-    <h1>From Livewire</h1>
-</div>
+<form action="#" class="md:w-1/2 space-y-5">
+
+    <div>
+        <x-input-label for="title" :value="__('Vacancy Title')" />
+        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"
+            placeholder="Title" />
+        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="salary" :value="__('Monthly Salary')" />
+        <select name="salary" id="salary"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="category" :value="__('Category')" />
+        <select name="category" id="category"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="company" :value="__('Company')" />
+        <x-text-input id="company" class="block mt-1 w-full" type="text" name="company" :value="old('company')"
+            placeholder="Company Name" />
+        <x-input-error :messages="$errors->get('company')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="deadline" :value="__('Deadline')" />
+        <x-text-input id="deadline" class="block mt-1 w-full" type="date" name="deadline" :value="old('deadline')" />
+        <x-input-error :messages="$errors->get('deadline')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="description" :value="__('Description')" />
+        <textarea name="description" id="description" cols="30" rows="10"
+            class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full"></textarea>
+        <x-input-error :messages="$errors->get('description')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="image" :value="__('Image')" />
+        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image" />
+        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+    </div>
+
+    <x-primary-button>
+        {{ __('Create Vacancy') }}
+    </x-primary-button>
+
+</form>
