1d34a83133d6fd2fa969ea6be3317f75fc4f6d94
lain <lyx@lyxincyberia.org>
2024-09-28 13:55:54 -0500

    added paginatin

--- resources/views/livewire/show-vacancies.blade.php
+++ resources/views/livewire/show-vacancies.blade.php
@@ -1,35 +1,41 @@
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-    @forelse ($vacancies as $vacancy)
-        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-cent">
-            <div class="space-y-3">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacancy->title }}
-                </a>
+        @forelse ($vacancies as $vacancy)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-cent">
+                <div class="space-y-3">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacancy->title }}
+                    </a>
 
-                <p class="text-sm text-gray-600 font-bold">{{ $vacancy->company }}</p>
-                <p class="text-sm">Deadline: {{ $vacancy->deadline }}</p>
+                    <p class="text-sm text-gray-600 font-bold">{{ $vacancy->company }}</p>
+                    <p class="text-sm">Deadline: {{ $vacancy->deadline }}</p>
+                </div>
+
+                <div class="flex flex-col md:flex-row stretch gap-3 mt-5 md:mt-0">
+                    <a href="#"
+                        class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Candidates
+                    </a>
+
+                    <a href="#"
+                        class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Edit
+                    </a>
+
+                    <a href="#"
+                        class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Delete
+                    </a>
+                </div>
             </div>
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">There are no vacancies to show</p>
+        @endforelse
 
-            <div class="flex flex-col md:flex-row stretch gap-3 mt-5 md:mt-0">
-                <a href="#"
-                    class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Candidates
-                </a>
+    </div>
 
-                <a href="#"
-                    class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Edit
-                </a>
-
-                <a href="#"
-                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                    Delete
-                </a>
-            </div>
-        </div>
-    @empty
-        <p class="p-3 text-center text-sm text-gray-600">There are no vacancies to show</p>
-    @endforelse
-
+    <div class="mt-10">
+        {{ $vacancies->links() }}
+    </div>
 </div>
