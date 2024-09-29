06ef94bfa16356aa4374a4df7a5e758c9ab6eb66
lain <lyx@lyxincyberia.org>
2024-09-29 13:24:10 -0500

    started to work in vacancy edit

--- resources/views/livewire/show-vacancies.blade.php
+++ resources/views/livewire/show-vacancies.blade.php
@@ -18,7 +18,7 @@
                         Candidates
                     </a>
 
-                    <a href="#"
+                    <a href="{{ route('vacancies.edit', ['vacancy' => $vacancy]) }}"
                         class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
                         Edit
                     </a>
