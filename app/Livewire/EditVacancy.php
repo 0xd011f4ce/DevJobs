87a673d76f9c97d229a40caa41767199de7650ff
lain <lyx@lyxincyberia.org>
2024-09-30 18:59:44 -0500

    the mount livewire method

--- app/Livewire/EditVacancy.php
+++ app/Livewire/EditVacancy.php
@@ -4,10 +4,20 @@ namespace App\Livewire;
 
 use App\Models\Categories;
 use App\Models\Salaries;
+use App\Models\Vacancy;
 use Livewire\Component;
 
 class EditVacancy extends Component
 {
+    public $title;
+
+    public function mount(Vacancy $vacancy)
+    {
+        // This method is called when the component is initialized
+        // vacancy is the model that is passed to the component
+        $this->title = $vacancy->title;
+    }
+
     public function render()
     {
         $salaries = Salaries::all();
